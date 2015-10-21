f4754998bd055af75ab3c1123b124185cd91dcb6
Ramón Albertí <ralberti@mequedouno.com>
2015-10-21 16:24:14 +0200

    config.dist

--- config/settings.dist.js
+++ config/settings.dist.js
@@ -4,17 +4,6 @@ var settings = {
     appMode    : 'test',
     path       : path.normalize(path.join(__dirname, '..')),
     port       : process.env.NODE_PORT || 3000,
-    database   : {
-        protocol : "mysql",
-        host     : "127.0.0.1",
-        database : "your_database",
-        user     : "your_user",
-        password : "your_password",
-        query    : {
-            pool : false, //CACHE querys
-            debug: true
-        }
-    },
     logsdir     : __dirname + '/../logs/access.log',
     secret_jwt  : "crypt_key123",
 
