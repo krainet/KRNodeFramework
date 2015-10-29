5adb2056be927dcd8e29af8ff757965767412dfa
Ramón Albertí <ralberti@mequedouno.com>
2015-10-29 13:52:54 +0100

    Setup APN & gitignore

--- app/helpers/push_helper.js
+++ app/helpers/push_helper.js
@@ -3,6 +3,8 @@
  */
 var gcm         = require('node-gcm');
 var settings    = require('../../config/settings');
+var apn         = require('apn');
+
 
 var push_helper = {
     Android : {
@@ -11,44 +13,67 @@ var push_helper = {
     Apple : {
 
     },
-    SendOnePush : function(token,title,message,cb){
-        if(!token)
-            return cb('No device token',null);
+    SendOnePush : function(token,title,message,platform,cb){
 
-        var message = new gcm.Message({
-            collapseKey: "demo",
-            priority: "high",
-            contentAvailable: true,
-            delayWhileIdle: true,
-            timeToLive: 3,
-            //restrictedPackageName: "somePackageName",
-            dryRun: false,
-            data: {
-                "Nick": "Krainet",
-                "From": "MQ1"
-            },
-            notification: {
-                body: message,
-                title: title,
-                msg: message,
-                icon: "ic_launcher"
-            },
-            msg: 'Test123'
-        });
+        switch(platform){
+            case 1:
+                var callback = function(errorNum, notification){
+                    console.log('Error is: %s', errorNum);
+                    console.log("Note " + notification);
+                }
+                settings.apn_options.callback=callback;
+                var note = new apn.Notification();
+                note.badge = 1;
+                note.sound = "notification-beep.wav";
+                note.alert = { "body" : "Your turn!", "action-loc-key" : "Play" , "launch-image" : "mysplash.png"};
+                note.alert.body=message;
+                note.payload = {'messageFrom': 'MeQuedoUno'};
+                var apnConnection = new apn.Connection(settings.apn_options);
+                apnConnection.pushNotification(note, token);
+                break;
+            case 2:
+                //ANDROID
+                var message = new gcm.Message({
+                    collapseKey: "demo",
+                    priority: "high",
+                    contentAvailable: true,
+                    delayWhileIdle: true,
+                    timeToLive: 3,
+                    //restrictedPackageName: "somePackageName",
+                    dryRun: false,
+                    data: {
+                        "Nick": "Krainet",
+                        "From": "MQ1"
+                    },
+                    notification: {
+                        body: message,
+                        title: title,
+                        msg: message,
+                        icon: "ic_launcher"
+                    },
+                    msg: 'Test123'
+                });
 
-        var regIds = [];
-        regIds.push(token);
+                var regIds = [];
+                regIds.push(token);
 
-        message.addData({
-            key1: 'message1',
-            key2: 'message2'
-        });
+                message.addData({
+                    key1: 'message1',
+                    key2: 'message2'
+                });
 
-        // Set up the sender with you API key
-        var sender = new gcm.Sender(settings.push_key_gcm);
-        sender.sendNoRetry(message, { registrationIds: regIds }, function(err, result) {
-            return cb(err,result);
-        });
+                // Set up the sender with you API key
+                var sender = new gcm.Sender(settings.push_key_gcm);
+                sender.sendNoRetry(message, { registrationIds: regIds }, function(err, result) {
+                    return cb(err,result);
+                });
+                break;
+            default:
+                return cb('Error in selected platform',null);
+                break;
+        }
+
+
     }
 }
 
