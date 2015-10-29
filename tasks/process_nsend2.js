d29de745c867cb6b730d0787d33a8f1036ac8683
Marc Mateu <mmateu@mequedouno.com>
2015-10-29 13:29:18 +0100

    canvis per tot arreu

--- tasks/process_nsend2.js
+++ tasks/process_nsend2.js
@@ -82,7 +82,7 @@ var sender = function() {
             var updateMessage = {
                 method: 'POST',
                 uri: API_URL+'/messages/'+ this.message.id,
-                body: {links: this.message.links},
+                body: {links: this.message.links, text: this.html.replace(/(<([^>]+)>)/ig,"") },
                 json: true,
                 headers: {
                     Authorization: 'Basic ' + this.token
